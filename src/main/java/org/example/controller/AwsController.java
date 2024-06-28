f9182a8b964bd0d85e7f401428c07e07a959cc63
TAEUNG JEON <jtw10155@naver.com>
2024-06-28 15:34:59 +0900

    move region6

--- src/main/java/org/example/controller/AwsController.java
+++ src/main/java/org/example/controller/AwsController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AwsController {
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "he llo w orld";
+        return "hello world";
     }
 }
